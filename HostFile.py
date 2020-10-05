c75374b34f8142f2eb212a2d1f664411629e876d
Alper Umut Uçar <alper.ucar@useinsider.com>
2020-10-05 14:19:35 +0300

    data tables are read for the static apis and the host file can be created as expected!

--- HostFile.py
+++ HostFile.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QSystemTrayIcon, QAction, QMenu
+from PyQt5.QtWidgets import QWidget, QSystemTrayIcon, QAction, QMenu, QAbstractButton, QPushButton, QTextEdit, QListWidget, QLineEdit
 from PyQt5.QtGui import QIcon
-
+import shutil, os, sys, platform
+import pymysql
 
 class Ui_HostFileChange(QWidget):
 
@@ -20,28 +21,28 @@ class Ui_HostFileChange(QWidget):
         self.tray_icon.setContextMenu(self.tray_menu)
 
         HostFileChange.setObjectName("HostFileChange")
-        HostFileChange.resize(433, 498)
+        HostFileChange.resize(450, 500)
         HostFileChange.setStyleSheet("background-color: rgb(0, 0, 0);")
 
         self.centralwidget = QtWidgets.QWidget(HostFileChange)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.ipText = QtWidgets.QTextEdit(self.centralwidget)
+        self.ipText = QTextEdit(self.centralwidget)
         self.ipText.setGeometry(QtCore.QRect(20, 30, 191, 31))
         self.ipText.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.ipText.setObjectName("ipText")
 
-        self.ipButton = QtWidgets.QPushButton(self.centralwidget)
+        self.ipButton = QPushButton(self.centralwidget)
         self.ipButton.setGeometry(QtCore.QRect(260, 410, 121, 31))
         self.ipButton.setStyleSheet("background-color: rgb(24, 186, 118);")
         self.ipButton.setObjectName("ipButton")
 
-        self.clearButton = QtWidgets.QPushButton(self.centralwidget)
-        self.clearButton.setGeometry(QtCore.QRect(20, 412, 111, 31))
-        self.clearButton.setStyleSheet("background-color: rgb(24, 186, 118);")
-        self.clearButton.setObjectName("clearButton")
+        self.clear = QPushButton(self.centralwidget)
+        self.clear.setGeometry(QtCore.QRect(20, 412, 111, 31))
+        self.clear.setStyleSheet("background-color: rgb(24, 186, 118);")
+        self.clear.setObjectName("clearButton")
 
-        self.generateButton = QtWidgets.QPushButton(self.centralwidget)
+        self.generateButton = QPushButton(self.centralwidget)
         self.generateButton.setGeometry(QtCore.QRect(260, 330, 121, 31))
         self.generateButton.setStyleSheet("background-color: rgb(24, 186, 118);")
         self.generateButton.setObjectName("generateButton")
@@ -51,10 +52,14 @@ class Ui_HostFileChange(QWidget):
         self.nameText.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.nameText.setObjectName("nameText")
 
-        self.searchButton = QtWidgets.QPushButton(self.centralwidget)
+        self.searchButton = QPushButton(self.centralwidget)
         self.searchButton.setGeometry(QtCore.QRect(260, 90, 121, 31))
         self.searchButton.setStyleSheet("background-color: rgb(24, 186, 118);")
         self.searchButton.setObjectName("searchButton")
+
+        self.changeIp = QtWidgets.QPushButton("ipButton")
+        self.generate = QtWidgets.QPushButton("generateButton")
+        self.search = QtWidgets.QPushButton("searchButton")
 
         self.ipLabel = QtWidgets.QLabel(self.centralwidget)
         self.ipLabel.setGeometry(QtCore.QRect(20, 10, 101, 16))
@@ -70,6 +75,10 @@ class Ui_HostFileChange(QWidget):
         self.changedLabel.setGeometry(QtCore.QRect(260, 140, 141, 16))
         self.changedLabel.setStyleSheet("color: rgb(255, 255, 255);")
         self.changedLabel.setObjectName("changedLabel")
+
+
+        self.view = QListWidget(self)
+        Functions.listing(self.view)
 
         self.mainList = QtWidgets.QTreeView(self.centralwidget)
         self.mainList.setGeometry(QtCore.QRect(20, 150, 191, 231))
@@ -95,6 +104,9 @@ class Ui_HostFileChange(QWidget):
         QtCore.QMetaObject.connectSlotsByName(HostFileChange)
         self.tray_icon.show()
 
+        self.clear.clicked.connect(lambda: Functions.click('', ''))
+        self.ipButton.clicked.connect(lambda: Functions.change_click(self, False))
+
     def update_display(self, text):
 
         for widget in self.widgets:
@@ -107,16 +119,206 @@ class Ui_HostFileChange(QWidget):
         _translate = QtCore.QCoreApplication.translate
         HostFileChange.setWindowTitle(_translate("HostFileChange", "MainWindow"))
         self.ipButton.setText(_translate("HostFileChange", "Change IP"))
-        self.clearButton.setText(_translate("HostFileChange", "Clear the Host File"))
+        self.clear.setText(_translate("HostFileChange", "Clear the Host File"))
         self.generateButton.setText(_translate("HostFileChange", "Generate Partner"))
         self.searchButton.setText(_translate("HostFileChange", "Search Partner"))
         self.ipLabel.setText(_translate("HostFileChange", "Enter Machine IP:"))
         self.nameLabel.setText(_translate("HostFileChange", "Enter Partner Name:"))
         self.changedLabel.setText(_translate("HostFileChange", "Partners that will be changed"))
 
+    def set_static_api(ip, task_id):
+
+        host = open("hosts", 'a')
+        #host.write(ip + " " + task_id + ".static.api.useinsider.com" + "\n\n") UNNECESSARY
+        host.close()
+
+    def open_and_write_partner(ip, partner_name):
+
+        host = open("hosts", 'a')
+        site_list = [
+            partner_name + ".inone.useinsider.com",
+            partner_name + ".panel.sociaplus.com",
+            partner_name + ".api.sociaplus.com",
+            partner_name + ".panel.useinsider.com",
+            partner_name + ".api.useinsider.com"
+        ]
+
+        for i in site_list:
+            host.write(ip + ' ' + i + '\n')
+
+        host.write("\n")
+        host.close()
+
+
+class Functions():
+
+    def __init__(self):
+        pass
+
+    def click(ip, sites):
+        "Working directory is needed for moving command in order to move the newly created host file"
+        project_direct = os.path.join(os.getcwd(), "hosts")
+
+        "Check the OS for host file destination directory and command"
+        if platform.system() == "Linux":
+            "For linux host file directory is constant"
+            host_direct = '/etc/hosts'
+            command = 'mv'
+            prefix = 'pkexec '
+
+        elif platform.system() == "Windows":
+            host_direct = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+            command = 'move'
+            prefix = ''
+
+        else:
+            return "OS cannot be identified. This program only runs at Linux and Windows"
+
+        print("alper")
+
+        "If the ip is given then the rest of the change function executes. Otherwise host file will be empty"
+        if (ip != ''):
+
+            Functions.new_write_file(5)
+            "Check if the ip is in right format"
+            dot = ip.count('.')
+            if (dot != 3):
+                return "Ip is not in right format"
+
+            "Check if the partner list is not empty"
+            if (sites != []):
+
+                "for every site in sites list the ip is added"
+                #for site in sites:
+                    #Functions.open_and_write_partner(ip, site)
+
+            "Ip will always prepended to all constant links"
+            print("aaaaaa")
+            Functions.add_sabit_links(ip)
+
+        else:
+            "This part executes when the user did not enter ip or pressed clear button on UI"
+            "If the ip is not given then the user wants to clear their host file"
+            Functions.new_write_file(5)
+
+        "Host file is fully created with the user requests. Move command is executed"
+        #os.system(prefix + command + ' ' + project_direct + ' ' + host_direct)
+        #shutil.move(host_direct)
+
+
+    def listing(view):
+
+        "Check if the sites.txt file exists"
+        if os.path.isfile("sites.txt"):
+            a = 0
+            file = open("sites.txt", "r")
+
+            for i in file:
+                "removes '\n' from the end of the strings"
+                text = i[:-1]
+                view.insertItem(a, text)
+                a += 1
+
+    def add_partner(view, new_partner):
+
+        new_part = new_partner.text()
+        file = open("sites.txt", "a")
+        file.write(new_part + "\n")
+
+        size = len(view)
+        view.insertItem(size, new_part)
+        new_partner = ""
+
+    def update_partners(partners):
+        """
+        :type partners: list <QListWidgetItem>
+        """
+        file = open("sites.txt", "w")
+
+        "write partners one by one"
+        for i in range(len(partners)):
+            file.write(partners[i].text() + '\n')
+
+    def new_write_file(self):
+
+        if platform.system() == "Linux":
+            host = open("hosts", "w")
+            print("Linux")
+            sites = [
+                "127.0.0.1	localhost",
+                "127.0.1.1	insider",
+                "::1     ip6-localhost ip6-loopback",
+                "fe00::0 ip6-localnet",
+                "ff00::0 ip6-mcastprefix",
+                "ff02::1 ip6-allnodes",
+                "ff02::2 ip6-allrouters"
+            ]
+        elif platform.system() == "Windows":
+            host = open("hosts", "a")
+            host.truncate(0)
+            print("Windows")
+            sites = [
+                "127.0.0.1	localhost",
+                "127.0.1.1	insider",
+                "::1     ip6-localhost ip6-loopback",
+                "fe00::0 ip6-localnet",
+                "ff00::0 ip6-mcastprefix",
+                "ff02::1 ip6-allnodes",
+                "ff02::2 ip6-allrouters"
+            ]
+
+        for s in sites:
+            host.write(s + '\n')
+        host.write("\n")
+        host.close()
+
+    def change_click(self, local):
+        if not local:
+            ip = self.ipText.toPlainText()
+            sites = []
+            for i in range(self.view.count()): #hata burada
+                print("alper")
+                sites.append(str(self.view.item(i).text())) #instead of text I should have my list from the database
+        elif local:
+            ip = "127.0.0.1"
+            sites = []
+            for i in range(self.view.count()):
+                sites.append(str(self.view.item(i).text()))
+        Functions.click(ip, sites)
+
+    def add_sabit_links(ip):
+        print("alper")
+        host = open("hosts", "a")
+        site_list = Functions.get_static_list()
+        if ip == '127.0.0.1':
+            site_list.append("local.static.api.useinsider.com")
+
+        for i in site_list:
+            host.write(ip + ' ' + i + '\n')
+
+        host.write("\n")
+        host.close()
+
+    def get_static_list():
+        db = pymysql.connect(host="qa-insider-rds.cdu0cfttyttl.eu-west-1.rds.amazonaws.com",
+                             user="",
+                             password="",
+                             port=3306,
+                             db="spPanel",
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM hosts_static_url")
+        url = cursor.fetchall()
+        site_list = []
+        for i in url:
+            site_list.append(i["url"])
+        return site_list
+
 
 if __name__ == "__main__":
     import sys
+    Functions.get_static_list()
     app = QtWidgets.QApplication(sys.argv)
     HostFileChange = QtWidgets.QMainWindow()
     ui = Ui_HostFileChange()
